591111daa4eddbc2f3b0c2b50b4c4c4765c0ff98
jennybsk <jekje9380@oslomet.no>
2024-04-11 16:28:04 +0200

    oppdatert js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,89 +1,42 @@
-// Funksjon for kjøp av billett
-function kjøpAvBillett() {
-    // Tømmer feilmeldingene før validering
-    $("#ugyldigAntall").html("");
-    $("#ikkeNummer").html("");
-    $("#ugyldigFornavn").html("");
-    $("#ugyldigEtternavn").html("");
-    $("#ugyldigEpost").html("");
-
-    const film = $("#filmer").val();
-    const antall = $("#antall").val();
-    const fornavn = $("#fornavn").val();
-    const etternavn = $("#etternavn").val();
-    const telefonnr = $("#telefonnr").val();
-    const epost = $("#epost").val();
-
-
-    if (antall === "" || isNaN(antall) || parseInt(antall) <= 0) {
-        $("#ugyldigAntall").html("Ugyldig, må fylle inn antall");
-    }
-
-    if(isNaN(telefonnr) || telefonnr.length !== 8){
-        $("#ikkeNummer").html("Ugyldig, telefonnr må bestå av 8 siffer");
-    }
-
-    if (fornavn.length === 0 || !isNaN(fornavn)) {
-        $("#ugyldigFornavn").html("Ugyldig, må fylle inn fornavn");
-    }
-
-    if (etternavn.length === 0 || !isNaN(etternavn)) {
-        $("#ugyldigEtternavn").html("Ugyldig, må fylle inn etternavn");
-    }
-
-    if (!epost.trim() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(epost)) {
-        $("#ugyldigEpost").html("Ugyldig e-postadresse");
-    }
-
-    else {
-        const registrert = {
-            film: film,
-            antall: antall,
-            fornavn: fornavn,
-            etternavn: etternavn,
-            telefonnr: telefonnr,
-            epost: epost
-        };
-
-        // Registrerer den nye infoen i arrayet
-        kinobillettRegister.push(registrert);
-
-        // Viser den nye infoen i arrayet
-        visKinobillettRegister();
-
-        // Sletter info fra input-boksene
-        $("#filmer").val("");
-        $("#antall").val("");
-        $("#fornavn").val("");
-        $("#etternavn").val("");
-        $("#telefonnr").val("");
-        $("#epost").val("");
-    }
+function regKinobillett() {
+    const kinobillett = {
+    filmer : $("#filmer").val(),
+    antall : $("#antall").val(),
+    fornavn : $("#fornavn").val(),
+    etternavn : $("#etternavn").val(),
+    telefonnr : $("#telefonnr").val(),
+    epost : $("#epost").val(),
+};
+    $.post("/lagre", kinobillett, function(){
+    hentAlle();
+});
+    $("#filmer").val("");
+    $("#antall").val("");
+    $("#fornavn").val("");
+    $("#etternavn").val("");
+    $("#telefonnr").val("");
+    $("#epost").val("");
 }
 
-function hentAlle() {
-    $.get("/hentAlle", function(kinobillettRegister) {
-        formaterData(kinobillettRegister);
+    function hentAlle() {
+    $.get("/hentAlle", function(billetter) {
+        formaterData(billett);
     });
 }
 
-// Skriver ut array med registrerte
-function visKinobillettRegister(kinobillettRegister) {
-    let ut = "<table><tr>" +
-        "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" +
-        "</tr>";
-    for (let r of kinobillettRegister) {
-        ut += "<tr>";
-        ut += "<td>" + r.film + "</td><td>" + r.antall + "</td><td>" + r.fornavn + "</td><td>" + r.etternavn + "</td><td>" + r.telefonnr + "</td><td>" + r.epost + "</td>";
-        ut += "</tr>";
-    }
+    function formaterData(billetter) {
+    let ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th>" +
+    "<th>Etternavn</th><th>Telefonnr</th><th>Epost</th></tr>";
+    for (const billett of billetter) {
+    ut += "<tr><td>" + billett.filmer + "</td><td>" + billett.antall + "</td><td>" + billett.fornavn + "</td>" +
+    "<td>" + billett.etternavn + "</td><td>" + billett.telefonnr + "</td><td>" + billett.epost + "</td></tr>";
+}
     ut += "</table>";
     $("#kinobillettRegister").html(ut);
 }
 
-// Funksjon for å tømme arrayet for all registrert info
-function slettAlle() {
-    $.get("/slettAlle", function () {
-       hentAlle();
+    function slettAlle() {
+    $.get( "/slettAlle", function() {
+        hentAlle();
     });
 }
