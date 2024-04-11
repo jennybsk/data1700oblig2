170681d0253ad41c922433b6de8c9c6b43568d32
jennybsk <jekje9380@oslomet.no>
2024-04-11 11:49:41 +0200

    Oppdatert readme, første commit

---
+++ src/main/java/com/example/data1700oblig2/Data1700Oblig2Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Oblig2Application.class, args);
+    }
+
+}
